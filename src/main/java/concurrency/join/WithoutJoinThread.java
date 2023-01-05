3a8331dab38a4342669d55336faf861604754c51
nkchauhan003 <nkchauhan003@gmail.com>
2023-01-05 15:11:56 +0530

    added cc

---
+++ src/main/java/concurrency/join/WithoutJoinThread.java
@@ -0,0 +1,14 @@
+package concurrency.join;
+
+public class WithoutJoinThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 5; i++)
+                System.out.println(Thread.currentThread().getName()+": "+i);
+        });
+        thread.start();
+        //thread.join();
+        for (int i = 0; i < 5; i++)
+            System.out.println(Thread.currentThread().getName()+": "+i);
+    }
+}
