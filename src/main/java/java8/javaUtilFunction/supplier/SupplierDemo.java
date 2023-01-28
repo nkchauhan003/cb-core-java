d37f1f76608bbf41cedbb844242a7af00cc1a73d
nkchauhan003 <nkchauhan003@gmail.com>
2023-01-28 10:47:09 +0530

    added java 8 code

--- src/main/java/java8/javaUtilFunction/supplier/SupplierDemo.java
+++ src/main/java/java8/javaUtilFunction/supplier/SupplierDemo.java
@@ -5,6 +5,6 @@ import java.util.function.Supplier;
 public class SupplierDemo {
     public static void main(String[] args) {
         Supplier<Double> randomNumber = () -> Math.random();
-        System.out.println(randomNumber.get());
+        System.out.println(randomNumber.get()); // 0.5528909748432393
     }
 }
