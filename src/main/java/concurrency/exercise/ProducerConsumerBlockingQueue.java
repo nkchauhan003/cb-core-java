fb628977be1d749009f18f26c369d487dc2c2057
nkchauhan003 <nkchauhan003@gmail.com>
2023-02-02 15:33:56 +0530

    consumer producer problem

--- src/main/java/concurrency/exercise/ProducerConsumerBlockingQueue.java
+++ src/main/java/concurrency/exercise/ProducerConsumerBlockingQueue.java
@@ -6,26 +6,60 @@ import java.util.concurrent.BlockingQueue;
 public class ProducerConsumerBlockingQueue {
     public static void main(String[] args) {
         BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
-        Thread producer = new Thread(() -> {
+        Thread producer1 = new Producer(queue);
+        Thread producer2 = new Producer(queue);
+
+        Thread consumer1 = new Consumer(queue);
+        Thread consumer2 = new Consumer(queue);
+
+        producer1.start();
+        producer2.start();
+
+        consumer1.start();
+        consumer2.start();
+    }
+
+    private static class Producer extends Thread {
+
+        private BlockingQueue<String> queue;
+
+        public Producer(BlockingQueue<String> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
             while (true) {
                 try {
                     queue.put("Something !!!");
+                    System.out.println(
+                            "Produced: " + currentThread().getName() + ", RemainingCapacity: " + queue.remainingCapacity());
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
-        });
-        Thread consumer = new Thread(() -> {
+        }
+    }
+
+    private static class Consumer extends Thread {
+
+        private BlockingQueue<String> queue;
+
+        public Consumer(BlockingQueue<String> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
             while (true) {
                 try {
+                    queue.take();
                     System.out.println(
-                            "Consumed: " + queue.take() + ", RemainingCapacity: " + queue.remainingCapacity());
+                            "Consumed: " + currentThread().getName() + ", RemainingCapacity: " + queue.remainingCapacity());
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
-        });
-        producer.start();
-        consumer.start();
+        }
     }
 }
