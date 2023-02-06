136d372d5f0f1770d98d0978cd9cc13f3effe2e1
nkchauhan003 <nkchauhan003@gmail.com>
2023-02-06 16:33:37 +0530

    consumer producer problem

--- src/main/java/concurrency/creation/ExtendThread.java
+++ src/main/java/concurrency/creation/ExtendThread.java
@@ -1,16 +1,20 @@
 package concurrency.creation;
 
-class ThreadB extends Thread {
-    @Override
-    public void run() {
-        System.out.println("Thread started: " + this.getName());
-        System.out.println("Thread started: " + currentThread().getName());
-    }
-}
-
 public class ExtendThread {
     public static void main(String[] args) {
+
+        // instance of Thread's subclass
         Thread thread = new ThreadB();
+
+        // starting a thread by calling start() on subclass
         thread.start();
+    }
+
+    // extending 'java.lang.Thread'
+    private static class ThreadB extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Thread started: " + this.getName());
+        }
     }
 }
--- src/main/java/concurrency/creation/ImplementRunnable.java
+++ src/main/java/concurrency/creation/ImplementRunnable.java
@@ -1,15 +1,22 @@
 package concurrency.creation;
 
-class ThreadA implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Thread started: " + Thread.currentThread().getName());
-    }
-}
-
 public class ImplementRunnable {
     public static void main(String[] args) {
-        Thread thread = new Thread(new ThreadA());
+
+        // runnable instance
+        Runnable runnable = new RunnableImpl();
+
+        // thread instance
+        Thread thread = new Thread(runnable);
+
+        // starting a thread
         thread.start();
+    }
+
+    private static class RunnableImpl implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Thread started: " + Thread.currentThread().getName());
+        }
     }
 }
